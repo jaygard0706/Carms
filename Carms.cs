d1cdef475c5eb2922f06a1432e8562175cc9d22f
Jay Gardner <jaygard0706@gmail.com>
2026-03-16 01:15:01 -0400

    V1

---
+++ Carms.cs
@@ -0,0 +1,65 @@
+﻿using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using Quintessential;
+using Quintessential.Settings;
+using SDL2;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Carms
+{
+
+    public class Carms: QuintessentialMod
+    {
+		public static MethodInfo PublicMethod<T>(string method) => typeof(T).GetMethod(method, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+		public static MethodInfo PrivateMethod<T>(string method) => typeof(T).GetMethod(method, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+		public static QuintessentialMod MainClassAsMod;
+
+        public override void Load(){
+			Quintessential.Logger.Log("Carms Loaded mrow");
+			MainClassAsMod = this;
+    	}
+
+    	public override void PostLoad()
+		{
+			On.SolutionEditorScreen.method_50 += SES_Method_50;
+			On.class_153.method_221 += c153_Method_221;
+
+			On.SolutionEditorProgramPanel.method_221 += SolutionEditorProgramPanel_Method_221;
+
+			On.SolutionEditorScreen.method_511 += SES_Method_511;
+		}
+
+    	public override void Unload(){
+    	}
+
+    	public override void LoadPuzzleContent(){
+    		MetricDisplay.LoadPuzzleContent();
+    	}
+
+
+		public static float SES_Method_511(On.SolutionEditorScreen.orig_method_511 orig, SolutionEditorScreen SES_self)
+		{
+			return orig(SES_self);
+		}
+
+		public void SolutionEditorProgramPanel_Method_221(On.SolutionEditorProgramPanel.orig_method_221 orig, SolutionEditorProgramPanel SEPP_self, float param_5658)
+		{
+			orig(SEPP_self, param_5658);
+			MetricDisplay.SEPP_method_221(SEPP_self);
+		}
+
+		public void SES_Method_50(On.SolutionEditorScreen.orig_method_50 orig, SolutionEditorScreen SES_self, float param_5703)
+		{
+			orig(SES_self, param_5703);
+		}
+
+		public static void c153_Method_221(On.class_153.orig_method_221 orig, class_153 c153_self, float param_3616)
+		{
+			orig(c153_self, param_3616);
+		}
+    }
+}
---
+++ MetricDisplay.cs
@@ -0,0 +1,105 @@
+using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using Quintessential;
+using SDL2;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Carms
+{
+	using Texture = class_256;
+	using PartType = class_139;
+
+    public static class MetricDisplay
+    {
+    	//data structs, enums, variables
+    	private static Texture metric_overlay;
+
+    	public static Vector2 textureDimensions(Texture texture) => texture.field_2056.ToVector2();
+
+		private enum resource : byte
+		{
+			create,
+			COUNT,
+		}
+
+		public static PartType MechanismArm1() => class_191.field_1764;
+		public static PartType MechanismArm2() => class_191.field_1765;
+		public static PartType MechanismArm3() => class_191.field_1766;
+		public static PartType MechanismArm6() => class_191.field_1767;
+		public static PartType MechanismPiston() => class_191.field_1768;
+
+		public static bool PartIsArm(Part part) {
+			return  part.method_1159() == MechanismArm1() ||
+					part.method_1159() == MechanismArm2() ||
+					part.method_1159() == MechanismArm3() ||
+					part.method_1159() == MechanismArm6() ||
+					part.method_1159() == MechanismPiston();
+		}
+
+        private static void display_metric(string name, string value, Vector2 position, float offset = 0f)
+		{
+			//copied from method_2086()
+			Texture score_card = class_238.field_1989.field_99.field_706.field_746;
+			Texture text_gradient = class_238.field_1989.field_99.field_706.field_751;
+
+			position += new Vector2(Input.ScreenSize().X - 364.0f, 268f); // base position
+
+			var font = class_238.field_1990.field_2142;
+			class_135.method_272(score_card, position);
+			class_135.method_290(name.method_441(), position + new Vector2(offset - 46f, 6f), font, Color.White, (enum_0)1, 1f, 0.6f, float.MaxValue, float.MaxValue, -2, Color.Black, text_gradient, int.MaxValue, false, true);
+			class_135.method_290(value.method_441(), position + new Vector2(30f, 6f), font, Color.FromHex(3483687), (enum_0)1, 1f, 0.6f, float.MaxValue, float.MaxValue, 0, new Color(), (class_256)null, int.MaxValue, false, true);
+		}
+
+    	//---------------------------------------------------//
+    	//internal main methods
+
+    	//---------------------------------------------------//
+    	public static void SEPP_method_221(SolutionEditorProgramPanel SEPPSelf)
+    	{
+    		var SES = new DynamicData(SEPPSelf).Get<SolutionEditorScreen>("field_2007");
+			bool simRunning = (SES.method_503() != enum_128.Stopped);
+
+			Vector2 metric_overlay_position = new Vector2(Input.ScreenSize().X - textureDimensions(metric_overlay).X - 300f, 296f - 32f);
+
+    		//-------------- compute metrics --------------//
+    		var maybeSim = Sim.method_1824(SES);
+    		bool validProgram = maybeSim.method_1085();
+
+    		// base metrics
+    		int cycles = SES.field_4017.method_1090(SES.method_2127());
+
+    		// program metrics
+    		int arms = 0;
+    		if (validProgram)
+    		{
+    			CompiledProgramGrid compiledProgramGrid = maybeSim.method_1087().method_1820();
+    			var programGridDict = new DynamicData(compiledProgramGrid).Get<Dictionary<Part, CompiledProgram>>("field_2368");
+    			arms = 0;
+    			foreach (var ENTRY in programGridDict)
+    			{
+					if(PartIsArm(ENTRY.Key)) arms++;
+    			}
+    		}
+
+    		float xpos;
+
+			// class_135.method_272(metric_overlay, metric_overlay_position);
+
+    		// METRIC 3:
+    		xpos = -670f;
+    		display_metric(class_134.method_253("Carms", string.Empty).method_1060() + ":", simRunning ? (cycles*arms).method_453() : "----", new Vector2(xpos, 0));
+    	}
+
+    	public static void LoadPuzzleContent()
+    	{
+    		//load texture
+    		string path = "carms/textures/";
+    		metric_overlay = class_235.method_615(path + "metric_overlay");
+    	}
+
+    	//---------------------------------------------------//
+    }
+}
